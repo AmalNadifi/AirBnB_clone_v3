22d57294f2b0f454bd76bd3f63b3ec3525785c2c
AmalNadifi <amal.nadifi9@gmail.com>
2023-12-04 10:59:41 -0800

    places view file updated

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -20,7 +20,7 @@ from models import storage
 # Retrieving all Place objects of a City
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def get_places_by_city(city_id):
+def places_by_city(city_id):
     '''
     The method retrieves the list of all Place objects of a City
 
@@ -44,7 +44,7 @@ def get_places_by_city(city_id):
 # Retrieving a specific Place object by ID
 @app_views.route('/places/<place_id>', methods=['GET'],
                  strict_slashes=False)
-def get_place(place_id):
+def places_by_id(place_id):
     '''
     This method retrieves a Place object
 
@@ -66,7 +66,7 @@ def get_place(place_id):
 
 # Deleting a specific Place object by ID
 @app_views.route('/places/<place_id>', methods=['DELETE'])
-def delete_place(place_id):
+def delete_places(place_id):
     '''
     This method deletes a Place object
 
@@ -92,7 +92,7 @@ def delete_place(place_id):
 # Creating a new Place object
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
-def create_place(city_id):
+def create_places(city_id):
     '''
     This method creates a Place object
 
@@ -114,24 +114,24 @@ def create_place(city_id):
         abort(400, 'Not a JSON')
 
     # Getting the JSON data from the request
-    data = request.get_json()
-    if 'user_id' not in data:
+    content = request.get_json()
+    if 'user_id' not in content:
         # Returning 400 error if 'user_id' key is missing in the JSON data
         abort(400, 'Missing user_id')
-    if 'name' not in data:
+    if 'name' not in content:
         # Returning 400 error if 'name' key is missing in the JSON data
         abort(400, 'Missing name')
 
     # Getting the User object with the given user_id from the storage
-    user = storage.get(User, data['user_id'])
+    user = storage.get(User, content['user_id'])
     if not user:
         # Returning 404 error if the User object is not found
         abort(404)
 
     # Assigning the city_id to the JSON data
-    data['city_id'] = city_id
+    content['city_id'] = city_id
     # Creating a new Place object with the JSON data
-    place = Place(**data)
+    place = Place(**content)
     # Saving the Place object to the storage
     place.save()
     # Returning the newly created Place object in JSON format with 201 status
@@ -140,7 +140,7 @@ def create_place(city_id):
 
 # Updating an existing Place object by ID
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def update_place(place_id):
+def update_places(place_id):
     '''
     This method updates a Place object
 
@@ -159,11 +159,11 @@ def update_place(place_id):
             abort(400, 'Not a JSON')
 
         # Getting the JSON data from the request
-        data = request.get_json()
-        ignore_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+        content = request.get_json()
+        keys_list = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
         # Updating the attributes of the Place object with the JSON data
-        for key, value in data.items():
-            if key not in ignore_keys:
+        for key, value in content.items():
+            if key not in keys_list:
                 setattr(place, key, value)
 
         # Saving the updated Place object to the storage
@@ -221,15 +221,15 @@ def places_search():
     if request.get_json() is None:
         abort(400, description="Not a JSON")
     # Extract data from the JSON request body
-    data = request.get_json()
+    content = request.get_json()
 
-    if data and len(data):
-        states = data.get('states', None)
-        cities = data.get('cities', None)
-        amenities = data.get('amenities', None)
+    if content and len(content):
+        states = content.get('states', None)
+        cities = content.get('cities', None)
+        amenities = content.get('amenities', None)
 
     # If no criteria provided, retrieve all places
-    if not data or not len(data) or (
+    if not content or not len(content) or (
             not states and
             not cities and
             not amenities):
